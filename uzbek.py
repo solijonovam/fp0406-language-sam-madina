11a45ff3a26ae33338f86cacf5702b542e7b332e
Madina Solijonova <solijonovam@berea.edu>
2023-05-02 01:23:24 -0400

    added the interpreter

--- uzbek.py
+++ uzbek.py
@@ -8,7 +8,7 @@ from stringsArrows import*
 # CONSTANTS
 #######################################
 
-RAQAMLAR = '0123456789' #digits
+DIGITS = '0123456789'
 
 
 #######################################
@@ -31,12 +31,36 @@ class Error:
 
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+        super().__init__(pos_start, pos_end, 'Noqonuniy xarakterr', details) #Illegal Character
 
 
 class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details=''):
-        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+        super().__init__(pos_start, pos_end, 'Yaroqsiz sintaksisx', details)   #Invalid Synta
+
+
+class RTError(Error):
+    def __init__(self, pos_start, pos_end, details, context):
+        super().__init__(pos_start, pos_end, 'Ish Vaqti Xatosi', details) #Runtime Error
+        self.context = context
+
+    def as_string(self):
+        result = self.generate_traceback()
+        result += f'{self.error_name}: {self.details}'
+        result += '\n\n' + stringsArrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+        return result
+
+    def generate_traceback(self):
+        result = ''
+        pos = self.pos_start
+        ctx = self.context
+
+        while ctx:
+            result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            pos = ctx.parent_entry_pos
+            ctx = ctx.parent
+
+        return 'Traceback (most recent call last):\n' + result
 
 
 #######################################
@@ -74,11 +98,10 @@ TT_SUZMOQ = 'SUZMOG'  #float
 TT_QOSHISH = 'QOSHISH'  #add
 TT_AJRASH= 'AJRASH' #minus
 TT_KOPAYTIRMOQ = 'KOPAYTIRMOQ' #multiply
-TT_BOLMOQ = 'BOLMOQ' #divde
+TT_BOLMOQ = 'BOLMOQ' #divide
 TT_OQAVS = 'OQAVS' #right parenthesis
 TT_CHQAVS = 'CHQAVS' #left parenthesis
 TT_FAILNYOHIRI= "FAINLYOHIRI" #end of the file
-
 
 class Token:
     def __init__(self, type_, value=None, pos_start=None, pos_end=None):
@@ -120,25 +143,25 @@ class Lexer:
         while self.current_char != None:
             if self.current_char in ' \t':
                 self.advance()
-            elif self.current_char in RAQAMLAR:
+            elif self.current_char in DIGITS:
                 tokens.append(self.make_number())
             elif self.current_char == '+':
-                tokens.append(Token(TT_QOSHISH))
+                tokens.append(Token(TT_QOSHISH, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '-':
-                tokens.append(Token(TT_AJRASH))
+                tokens.append(Token(TT_AJRASH, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '*':
-                tokens.append(Token(TT_KOPAYTIRMOQ))
+                tokens.append(Token(TT_KOPAYTIRMOQ, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '/':
-                tokens.append(Token(TT_BOLMOQ))
+                tokens.append(Token(TT_BOLMOQ, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '(':
-                tokens.append(Token(TT_OQAVS))
+                tokens.append(Token(TT_OQAVS, pos_start=self.pos))
                 self.advance()
             elif self.current_char == ')':
-                tokens.append(Token(TT_CHQAVS))
+                tokens.append(Token(TT_CHQAVS, pos_start=self.pos))
                 self.advance()
             else:
                 pos_start = self.pos.copy()
@@ -154,7 +177,7 @@ class Lexer:
         dot_count = 0
         pos_start = self.pos.copy()
 
-        while self.current_char != None and self.current_char in RAQAMLAR + '.':
+        while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
                 if dot_count == 1: break
                 dot_count += 1
@@ -177,6 +200,9 @@ class NumberNode:
     def __init__(self, tok):
         self.tok = tok
 
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
+
     def __repr__(self):
         return f'{self.tok}'
 
@@ -187,6 +213,9 @@ class BinOpNode:
         self.op_tok = op_tok
         self.right_node = right_node
 
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
+
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
 
@@ -195,6 +224,9 @@ class UnaryOpNode:
     def __init__(self, op_tok, node):
         self.op_tok = op_tok
         self.node = node
+
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
@@ -246,7 +278,7 @@ class Parser:
         if not res.error and self.current_tok.type != TT_FAILNYOHIRI:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected '+', '-', '*' or '/'"
+                "kutilgan '+', '-', '*' yoki '/'"
             ))
         return res
 
@@ -276,12 +308,12 @@ class Parser:
             else:
                 return res.failure(InvalidSyntaxError(
                     self.current_tok.pos_start, self.current_tok.pos_end,
-                    "Expected ')'"
+                    "kutilgan ')'"  #expected )
                 ))
 
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
-            "Expected int or float"
+            "kutilgan raqam yoki suzmoq" #expected int or float
         ))
 
     def term(self):
@@ -308,6 +340,142 @@ class Parser:
 
 
 #######################################
+# RUNTIME RESULT
+#######################################
+
+class RTResult:
+    def __init__(self):
+        self.value = None
+        self.error = None
+
+    def register(self, res):
+        if res.error: self.error = res.error
+        return res.value
+
+    def success(self, value):
+        self.value = value
+        return self
+
+    def failure(self, error):
+        self.error = error
+        return self
+
+
+#######################################
+# VALUES
+#######################################
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+        self.set_pos()
+        self.set_context()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def set_context(self, context=None):
+        self.context = context
+        return self
+
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value).set_context(self.context), None
+
+    def subbed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value).set_context(self.context), None
+
+    def multed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value).set_context(self.context), None
+
+    def dived_by(self, other):
+        if isinstance(other, Number):
+            if other.value == 0:
+                return None, RTError(
+                    other.pos_start, other.pos_end,
+                    'Nolga bolinish',  #Division by zero
+                    self.context
+                )
+
+            return Number(self.value / other.value).set_context(self.context), None
+
+    def __repr__(self):
+        return str(self.value)
+
+
+#######################################
+# CONTEXT
+#######################################
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+
+
+#######################################
+# INTERPRETER
+#######################################
+
+class Interpreter:
+    def visit(self, node, context):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.no_visit_method)
+        return method(node, context)
+
+    def no_visit_method(self, node, context):
+        raise Exception(f'No visit_{type(node).__name__} method defined')
+
+    ###################################
+
+    def visit_NumberNode(self, node, context):
+        return RTResult().success(
+            Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+        )
+
+    def visit_BinOpNode(self, node, context):
+        res = RTResult()
+        left = res.register(self.visit(node.left_node, context))
+        if res.error: return res
+        right = res.register(self.visit(node.right_node, context))
+        if res.error: return res
+
+        if node.op_tok.type == TT_QOSHISH:
+            result, error = left.added_to(right)
+        elif node.op_tok.type == TT_AJRASH:
+            result, error = left.subbed_by(right)
+        elif node.op_tok.type == TT_KOPAYTIRMOQ:
+            result, error = left.multed_by(right)
+        elif node.op_tok.type == TT_BOLMOQ:
+            result, error = left.dived_by(right)
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    def visit_UnaryOpNode(self, node, context):
+        res = RTResult()
+        number = res.register(self.visit(node.node, context))
+        if res.error: return res
+
+        error = None
+
+        if node.op_tok.type == TT_AJRASH:
+            number, error = number.multed_by(Number(-1))
+
+        if error:
+            return res.failure(error)
+        else:
+            return res.success(number.set_pos(node.pos_start, node.pos_end))
+
+
+#######################################
 # RUN
 #######################################
 
@@ -320,5 +488,11 @@ def run(fn, text):
     # Generate AST
     parser = Parser(tokens)
     ast = parser.parse()
+    if ast.error: return None, ast.error
 
-    return ast.node, ast.error
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
